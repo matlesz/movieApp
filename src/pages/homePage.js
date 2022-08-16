53f81736b752d28c374bc73f2a0578a2ca3e00a0
matlesz <matlesz@gmail.com>
2022-08-16 20:54:26 +0100

    Refactor: Template list page; HTTP API calls centralized

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,44 +1,32 @@
-import React, {useState, useEffect } from "react";
-import Header from "../components/headerMovieList";
-import MovieList from "../components/movieList";
-import FilterControls from "../components/filterControls";
+import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
-
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovies } from "../api/tmdb-api";
 
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        return json.results
-      })
-      .then(movies => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
-  const addToFavorites = movieId => {
-    // Find index position of selected movie in the list
-    const index = movies.map(m => m.id).indexOf(movieId);
 
-    StubAPI.add(movies[index]);
-    const updatedList = [...movies] ; // Make a copy of the movie list
-    updatedList.splice(index, 1);     // Remove selected movie from home page list
-    setMovies(updatedList)  
+  const addToFavorites = movieId => {
+    setMovies(movies => {
+      const index = movies.map(m => m.id).indexOf(movieId);
+      StubAPI.add(movies[index]);
+      let newMoviesState = [...movies]
+      newMoviesState.splice(index, 1);
+      return newMoviesState;
+    });
   };
 
   return (
-    <>
-      <Header numMovies={displayedMovies.length} />
-      <FilterControls onUserInput={handleChange} />
-      <MovieList
-        movies={displayedMovies}
+      <PageTemplate
+        title='All Movies'
+        movies={movies}
         buttonHandler={addToFavorites}
-      />     {/* CHANGED */}
-    </>
+      />
   );
 };
 
