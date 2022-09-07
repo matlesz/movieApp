cfe323d53d27d181646d21aae332b1f0c425f71f
matlesz <matlesz@gmail.com>
2022-09-07 07:07:05 +0100

    some additional stage 4 changes

---
+++ src/contexts/moviesContext.js
@@ -0,0 +1,55 @@
+import React, { useEffect, createContext, useReducer } from "react";
+import { getMovies } from "../api/tmdb-api";
+
+export const MoviesContext = createContext(null);
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-favorite":
+      return {
+        movies: state.movies.filter((m) => m.id !== action.payload.movie.id),
+        favorites: [...state.favorites, action.payload.movie],
+      };
+    case "load-movies":
+      return { movies: [...action.payload.movies], favorites: [] };
+    case "add-review":
+      // Completed in next section
+      break;
+    default:
+      return state;
+  }
+};
+
+const MoviesContextProvider = (props) => {
+  const [state, dispatch] = useReducer(reducer, { movies: [], favorites: [] });
+
+  const addToFavorites = (movieId) => {
+    const index = state.movies.map((m) => m.id).indexOf(movieId);
+    dispatch({ type: "add-favorite", payload: { movie: state.movies[index] } });
+  };
+
+  const addReview = () => {
+      // Completed in next section
+  };
+
+  useEffect(() => {
+    getMovies().then((movies) => {
+      dispatch({ type: "load-movies", payload: { movies } });
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <MoviesContext.Provider
+      value={{
+        movies: state.movies,
+        favorites: state.favorites,
+        addToFavorites: addToFavorites,
+        addReview: addReview,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
